262977879b12512bf7dafa1a4d1c0471fbe5d370
katey <katherine.nilan@gmail.com>
2017-06-18 19:36:43 -0700

    added a signup page/form, WIP adding a JS alert, further plans include adding a database to store sign ups

--- myflaskapp.py
+++ myflaskapp.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, \
+     request, url_for
+     
 app = Flask(__name__)
+
 
 @app.route("/")
 def home_page():
 	return render_template('home.html')
+
+
+@app.route('/SignUp')
+def showSignUp():
+
+	flash("You can't sign up yet, check back next week!")
+
+	return render_template('signup.html')
+
+# @app.route('/SignUp', methods=['POST'])
+# def attemptedSignIn():
+
+#     name = request.form['inputName']
+#     email = request.form['inputEmail']
+#     password = request.form['inputPassword']
+
+#     return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run()
